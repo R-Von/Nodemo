ff37197572403edc5d76e5ec7dd5ae950ad8dc18
R-Von <778615648@qq.com>
2020-03-19 15:59:47 +0800

    feat(): add fs system

--- demos/05_fs.js
+++ demos/05_fs.js
@@ -50,12 +50,138 @@
 
 
 // 2 fs.mkdir  创建目录
+// let fs = require('fs')
+
+
+// /**
+//  * 接收参数
+//  * path - 将创建的目录路径
+//  * mode - 目录权限（读写权限） 默认 0777
+//  * callback - 回调 传递异常参数 err 
+//  */
+// fs.mkdir('css',(err)=>{
+//   if(err){
+//     console.log(err)
+//     return false
+//   }else{
+//     console.log('mkdir success')
+//   }       
+// })
+
+// 3 fs.rmdir 删除目录
+// let fs = require('fs')
+
+// /**
+//  * 接收参数
+//  * path - 将要创建的目录路径
+//  * mode - 目录权限(读写权限) 默认 0777 
+//  * callback - 回调 传递异常参数  err
+//  */
+// fs.rmdir('css',(err)=>{
+//   if(err){
+//     console.log(err)
+//     return false
+//   }else{
+//     console.log('rmdir success')
+//   }
+// })
+
+// 4 fs.writeFile 写入文件
+// let fs = require('fs')
+
+// /**
+//  * filename (String) 文件名称
+//  * data (String|Buffer) 写入的内容 可以是字符串或者Buffer数据
+//  * - encoding(String) 可选。 默认 ‘utf-8’
+//  * - mode(Number) 文件读写权限 默认438
+//  * - flag (String) 默认值 ‘w’
+//  * callback {Function} 
+//  */
+
+//  fs.writeFile('index2.js','console.log("Hello World")',(err)=>{
+//    if(err){
+//     console.log(err)
+//     return false
+//    }else{
+//      console.log('write success')
+//    }
+//  })
+// 写入会清空
+
+// 05 appendFile 追加文件
+// let fs = require('fs')
+
+// fs.appendFile('text.txt',' add some word ',(err)=>{
+//   if(err){
+//     console.log(err)
+//     return false
+//   }else{
+//     console.log('append success')
+//   }
+// })
+
+// 没有的话 就会创建新的文件
+
+// 06 fs.readFile 读取文件  fs.readdir 读取目录  
+// let fs = require('fs')
+
+// fs.readFile('index.js',(err,data)=>{
+//   if(err){
+//     console.log(err)
+//     return false
+//   }else{
+//     console.log('success')
+//     console.log(data)    //Buffer格式
+//     console.log(data.toString())
+//   }
+// })
+
+// fs.mkdir('css',(err)=>{
+//   if(err){
+//     console.log(err)
+//     return false
+//   }else{
+//     console.log('success ')
+//   }
+// })
+
+// fs.writeFile('css/style.css','*{margin:0;padding:0}',(err)=>{
+//   if(err){
+//     console.log(err)
+//     return false
+//   }else{
+//     console.log('writeSuccess')
+//   }
+// })
+
+// fs.readdir('css',(err,data)=>{
+//   if(err){
+//     console.log(err)
+//     return false
+//   }else{
+//     console.log('readdir success')
+//     console.log(data)
+//   }
+// })
+
+// 07 fs.rename 重命名
 let fs = require('fs')
+// fs.rename('index2.js','test.js',(err)=>{
+//   if(err){
+//     console.log(err)
+//     return false
+//   }else{
+//     console.log('rename success')
+//   }
+// })
 
+// rename还可以起到剪切文件的功能
 
-/**
- * 接收参数
- * path - 将创建的目录路径
- * mode - 目录权限（读写权限） 默认 0777
- * callback - 回调 传递异常参数 err 
- */
+fs.rename('text.txt','css/text.css',(err)=>{
+  if(err){
+    console.log(err)
+    return false
+  }else{
+    console.log('rename success')
+  }
+})
